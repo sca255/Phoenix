8cb10080ca4fded091d107fa8331ddbee4538d71
arpan das <dasa20032009@gmail.com>
2024-09-03 08:28:09 +0530

    dep

---
+++ _settings/phoenix.custom.php
@@ -0,0 +1,38 @@
+<?php
+
+////	DO NOT MODIFY `phoenix.default.php`
+// Copy to `phoenix.custom.php` and modify there.
+
+////	General Database Options
+// Can be better overridden with a settings.custom.php file.
+$settings['db_host']             = 'mysql-scap2p-scap2p.d.aivencloud.com:16303';      /* ip or hostname to mysql server */
+$settings['db_user']             = 'avnadmin';      /* username used to connect to mysql */
+$settings['db_pass']             = 'AVNS_KeRzYNOpA8OzqNoEnS_';      /* password used to connect to mysql */
+$settings['db_name']             = 'defaultdb';      /* name of the Phoenix database */
+$settings['db_reset']            = false;            /* allow database to be reset in admin */
+
+////	Advanced Database Options
+$settings['db_prefix']           = 'ph_';    /* name prefixes for the Phoenix tables */
+$settings['db_persist']          = 'true';   /* use persistent connections if available. */
+
+////	General Tracker Options
+$settings['open_tracker']        = 'true'; /* track anything announced to it */
+$settings['announce_interval']   = 1800;             /* how often client will send requests */
+$settings['min_interval']        = 600;              /* how often client can force requests */
+$settings['default_peers']       = 50;               /* default # of peers to announce */
+$settings['max_peers']           = 100;              /* max # of peers to announce */
+
+////	Advanced Tracker Options
+$settings['external_ip']         = true;             /* allow client to specify ip address */
+$settings['default_compact']     = true;             /* force compact announces only */
+$settings['full_scrape']         = true;             /* allow scrapes without info_hash */
+$settings['random_limit']        = 500;              /* if peers > #, use alternate SQL RAND() */
+$settings['clean_with_requests'] = 1;                /* tweaks % of time tracker attempts idle peer removal */
+                                                     /* if you have a busy tracker, you may adjust this */
+                                                     /* example: 1 = 1%, 10 = 10%, 50 = 50%, 100 = every time */
+$settings['clean_with_cron']     = false;            /* should your tracker clean with cron */
+                                                     /* means clean_with_requests can be disabled for faster responses */
+$settings['honor_xff']           = false;            /* If this server is behind a frontend proxy, the client IP */
+                                                     /* will come in the form of a X-Forwarded-For. This option */
+                                                     /* should only be set if your frontend proxy properly handles */
+                                                     /* and filters XFF, else it allows for trivial IP spoofing */
